bbcb30aeec06fc0b99dfcadcdefc8ad39c719d1b
nyseer1 <nyseer.couse@gmail.com>
2026-08-03 00:10:46 -0400

    renamed index to server

--- app/api/listings/page.tsx
+++ app/api/listings/page.tsx
@@ -3,51 +3,8 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import BackToTopButton from "@/components/BackToTopButton";
-// export const runtime = "nodejs"; //make sure node is using full nodejs environment, not edge runtime (needed for mongodb)
-import dbConnect from '@/lib/dbConnect';
-import ListingModelDB from '@/models/Listing';
-import { API_BASE_URL } from '@/config';
-
-
-async function getUsers() {
-	try {
-		const response = await fetch(`${API_BASE_URL}/api/listings`);
-		const responseJSON = await response.json();
-		if (!responseJSON.ok) throw new Error('NetWork Error, Get Listings Failed');
-		return (
-
-
-			<>
-				{
-					responseJSON.map((eachEntity) => ( //name of var self explanatory
-						<div key={eachEntity._id.toString()} className="card">
-							<Image
-								src="/img_avatar.png"
-								alt="Listing"
-								width={100}
-								height={100}
-								style={{ width: "100%" }}
-							/>
-							<h4>
-								<b>{eachEntity.name}</b>
-							</h4>
-							<p>Blah blah blah stuff about the product</p>
-						</div>
-					))
-				}
-			</>);
-	}
-	catch (error) {
-		console.error('Fetch Failed: ', error);
-	}
-};
-
-getUsers();
 
 export default async function ListingsPage() {
-
-	const data = await getUsers();
-	if (!data) { return <h1>Failed to Load Data!</h1> }
 
 	const [isDesktop, setDesktop] = useState<boolean>(false);
 
